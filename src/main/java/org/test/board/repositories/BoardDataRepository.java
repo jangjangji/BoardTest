5d3524ef179e57af1cfce03caef600b30ab7fa80
jangjangji <sjjang41@naver.com>
2024-07-30 15:01:57 +0900

    설정 살짝 수정

--- src/main/java/org/test/board/repositories/BoardDataRepository.java
+++ src/main/java/org/test/board/repositories/BoardDataRepository.java
@@ -1,7 +1,8 @@
 package org.test.board.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.test.board.entities.BoardData;
 
-public interface BoardDataRepository extends JpaRepository<BoardData, Long>{
+public interface BoardDataRepository extends JpaRepository<BoardData, Long> {
 }
--- src/main/java/org/test/global/configs/MvcConfig.java
+++ src/main/java/org/test/global/configs/MvcConfig.java
@@ -2,8 +2,9 @@ package org.test.global.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableJpaAuditing
-public class MvcConfig {
+public class MvcConfig implements WebMvcConfigurer {
 }
